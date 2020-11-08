db5ddd023c29ec88c50e95e10f516952f529dbcc
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-08 10:01:29 +0530

    Added: Add function to the Linked List

---
+++ src/main/java/LinkedList.java
@@ -0,0 +1,38 @@
+
+public class LinkedList<K> {
+	public Node<K> head;
+	public Node<K> tail;
+	
+	public LinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+	
+	public void add(Node<K> newNode) {
+		if(this.tail == null) {
+			this.tail = newNode;
+		}
+		if(this.head == null) {
+			this.head = newNode;
+		}
+		else {
+			Node<K> tempNode = this.head;
+			this.head = newNode;
+			this.head.setNext(tempNode);
+		}
+	}
+	
+	public void printNodes() {
+		StringBuffer myNodes = new StringBuffer();
+		Node<K> tempNode = this.head;
+		while(tempNode.getNext() != null) {
+			myNodes.append(tempNode.getValue());
+			if(!tempNode.equals(this.tail)) {
+				myNodes.append("->");
+			}
+			tempNode = tempNode.getNext();
+		}
+		myNodes.append(tempNode.getValue());
+		System.out.println(myNodes);
+	}
+}
--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -2,6 +2,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class LinkedListTest {
+	
 	@Test
 	public void givenThreeNumbers_WhenLinked_ShouldPassesLinkedListTest() {
 		Node<Integer> firstNode = new Node<Integer>(56);
@@ -11,6 +12,22 @@ public class LinkedListTest {
 		secondNode.setNext(thirdNode);
 		boolean result = firstNode.getNext().equals(secondNode) &&
 						 firstNode.getNext().getNext().equals(thirdNode);
+		Assert.assertTrue(result);
+	}
+	
+	@Test
+	public void givenThreeNumbers_WhenAddedToLinkedList_ShouldBeAddedToTop() {
+		Node<Integer> firstNode = new Node<Integer>(70);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(56);
+		LinkedList<Integer> linkedList = new LinkedList<Integer>();
+		linkedList.add(firstNode);
+		linkedList.add(secondNode);
+		linkedList.add(thirdNode);
+		linkedList.printNodes();
+		boolean result = linkedList.head.equals(thirdNode) &&
+						 linkedList.head.getNext().equals(secondNode) &&
+						 linkedList.tail.equals(firstNode);
 		Assert.assertTrue(result);
 	}
 }
