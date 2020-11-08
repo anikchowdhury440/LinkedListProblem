50e09c13a5680d49ecfde810f69130b1fa29d417
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-08 13:22:47 +0530

    Added: Add function in linked List

--- src/main/java/LinkedList.java
+++ src/main/java/LinkedList.java
@@ -26,54 +26,6 @@ public class LinkedList<K> {
 			this.head.setNext(tempNode);
 		}
 	}
-	
-	public void append(Node<K> newNode) {
-		if(isEmpty()) {
-			this.tail = newNode;
-			this.head = newNode;
-		}
-		else {
-			this.tail.setNext(newNode);
-			this.tail = newNode;
-		}
-	}
-	
-	public void insert(Node<K> node, Node<K> newNode) {
-		Node<K> tempNode = this.head;
-		while(!tempNode.getValue().equals(node.getValue())) {
-			tempNode = tempNode.getNext();
-		}
-		newNode.setNext(tempNode.getNext());
-		tempNode.setNext(newNode);
-	}
-	
-	public K pop() {
-		Node<K> tempNode = this.head;
-		if(tempNode.equals(tail)) {
-			this.tail = null;
-			this.head = null;
-			return tempNode.getValue();
-		}
-		this.head = head.getNext();
-		return tempNode.getValue();
-	}
-
-	public K popLast() {
-		Node<K> tempNode = this.head;
-		Node<K> removedNode = this.tail;
-		if(tempNode.equals(tail)) {
-			this.tail = null;
-			this.head = null;
-			return removedNode.getValue();
-		}
-		while(tempNode.getNext() != this.tail)
-		{
-			tempNode = tempNode.getNext();
-		}
-		tempNode.setNext(null);
-		this.tail = tempNode;
-		return removedNode.getValue();
-	}
 
 	public void printNodes() {
 		StringBuffer myNodes = new StringBuffer();
